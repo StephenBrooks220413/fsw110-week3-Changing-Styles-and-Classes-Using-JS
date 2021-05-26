06aa9f2be09f42532e9dc4bfc36317b49b20afcf
stephenbrooks40 <stephenbrooks714@gmail.com>
2021-05-25 19:02:10 -0700

    updated files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < 6; i++) {
+    const ele = document.createElement("h2");
+    ele.innerHTML = "JavaScript";
+    ele.style.color = "cornflowerblue";
+    ele.style.fontSize = "20px";
+    ele.style.fontWeight = "lighter";
+    ele.style.fontFamily = "sans-serif";
+    ele.className = "border";
+    document.body.appendChild(ele);
+}
